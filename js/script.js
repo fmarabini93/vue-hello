e42f29af5a74faa1c887738160a4f7091756b674
fmarabini93 <fmarabini@icloud.com>
2021-05-17 14:49:30 +0200

    with-img-base

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ var root = new Vue(
       {
             el: "#root",
             data: {
-                  message: "Hello! Here's the first Vue exercise"
+                  message: "",
+                  image: "img/img.jpg"
             }
       }
 
