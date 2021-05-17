b1db1729e700570d89d8050663c9f8cd11fab64d
fmarabini93 <fmarabini@icloud.com>
2021-05-17 15:41:57 +0200

    with-css

--- js/script.js
+++ js/script.js
@@ -3,8 +3,31 @@ var root = new Vue(
       {
             el: "#root",
             data: {
-                  message: "",
-                  image: "img/img.jpg"
+                  message: "Spring",
+                  image: "img/spring.jpg"
+            },
+            methods: {
+                  changeSeason: function() {
+                        switch (this.message) {
+                              case "Spring":
+                                    this.message = "Summer";
+                                    this.image = "img/summer.jpg";
+                                    break;
+                              case "Summer":
+                                    this.message = "Fall";
+                                    this.image = "img/fall.jpg";
+                                    break;
+                              case "Fall":
+                                    this.message = "Winter";
+                                    this.image = "img/winter.jpg";
+                                    break;
+                              case "Winter":
+                                    this.message = "Spring";
+                                    this.image = "img/spring.jpg";
+                                    break;
+                        }
+
+                  }
             }
       }
 
